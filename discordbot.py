d4108717f27674b3cf0bdd0e9a71b21a53730acf
kuhaku989 <75543322+kuhaku989@users.noreply.github.com>
2021-01-11 17:02:41 +0900

    minecraft

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,31 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    if message.content == '/give_role':
+        member = message.author
+        role = member.guild.get_role(ID_ROLE_MEMBER)
+        await member.add_roles(role)
+        await message.delete()
+    if message.content == '/clean':
+        Ch = message.channel.id
+        if Ch == 796382696330756126 or Ch == 796383874069430282:
+            await message.channel.purge(limit=None)
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if not before.channel and after.channel:
+        set_mention_name = after.channel.name
+        role1 = discord.utils.get(member.guild.roles, name=set_mention_name)
+        await member.add_roles(role1)
+    elif before.channel and not after.channel:
+        remove_mention_name = before.channel.name
+        role1 = discord.utils.get(member.guild.roles, name=remove_mention_name)
+        await member.remove_roles(role1)
+
 
 bot.run(token)

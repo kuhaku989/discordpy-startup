87f9843f832db5d54f39ce2071805a02bd758b2c
kuhaku989 <75543322+kuhaku989@users.noreply.github.com>
2021-01-11 17:36:44 +0900

    Minecraft

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,37 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+import random
+import sys
+import asyncio
+import re
 
-
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
+ID_ROLE_MEMBER = 796380696812716102 # 付けたい役職のID(int)
 
-@bot.event
+@client.event
 async def on_message(message):
-    try:
-        if message..author.bot:
-            return
-        await bot.process_commands(message)
-    except Exception:
-        await meesage.channel.send(f'```\n{traceback.format_exc()}\n```')
-    
-@bot.commands
-async def ping(ctx):
-    await ctx.send('pong')
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    if message.content == '/give_role':
+        member = message.author
+        role = member.guild.get_role(ID_ROLE_MEMBER)
+        await member.add_roles(role)
+        await message.delete()
+    if message.content == '/clean':
+        Ch = message.channel.id
+        if Ch == 796382696330756126 or Ch == 796383874069430282:
+            await message.channel.purge(limit=None)
 
-@bot.commands
-async def neko(ctx):
-    await ctx.send('にゃーん')
+@client.event
+async def on_voice_state_update(member, before, after):
+    if not before.channel and after.channel:
+        set_mention_name = after.channel.name
+        role1 = discord.utils.get(member.guild.roles, name=set_mention_name)
+        await member.add_roles(role1)
+    elif before.channel and not after.channel:
+        remove_mention_name = before.channel.name
+        role1 = discord.utils.get(member.guild.roles, name=remove_mention_name)
+        await member.remove_roles(role1)
     
-bot.run(token)
+client.run(token)

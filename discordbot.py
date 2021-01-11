af806da0d26dea0c30e2bc1f94a1f7f2aaf65c8a
kuhaku989 <75543322+kuhaku989@users.noreply.github.com>
2021-01-12 00:15:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,39 +1,37 @@
 import discord
+from discord.ext import commands
 import random
 import sys
 import asyncio
 import re
 import os
 
-
 token = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
 ID_ROLE_MEMBER = 796380696812716102 # 付けたい役職のID(int)
 
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    if message.content == '/give_role':
-        member = message.author
-        role = member.guild.get_role(ID_ROLE_MEMBER)
-        await member.add_roles(role)
-        await message.delete()
-    if message.content == '/clean':
-        Ch = message.channel.id
-        if Ch == 796382696330756126 or Ch == 796383874069430282:
-            await message.channel.purge(limit=None)
+@bot.command()
+async def give_role(ctx):
+    member = message.author
+    role = member.guild.get_role(ID_ROLE_MEMBER)
+    await member.add_roles(role)
+    await message.delete()
 
-@client.event
+@bot.command()
+async def clean(ctx):
+    Ch = message.channel.id
+    if Ch == 796382696330756126 or Ch == 796383874069430282:
+        await message.channel.purge(limit=None)
+
+@bot.command()
 async def on_voice_state_update(member, before, after):
-    if not before.channel and after.channel:
+     if not before.channel and after.channel:
         set_mention_name = after.channel.name
         role1 = discord.utils.get(member.guild.roles, name=set_mention_name)
         await member.add_roles(role1)
-    elif before.channel and not after.channel:
+     elif before.channel and not after.channel:
         remove_mention_name = before.channel.name
         role1 = discord.utils.get(member.guild.roles, name=remove_mention_name)
         await member.remove_roles(role1)
     
-client.run(token)
+bot.run(token)
